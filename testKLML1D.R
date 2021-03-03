aa58a01b4c9513b3b38be1eeb277b1b604e47acf
Bern <48306596+Xiaoyang19@users.noreply.github.com>
2021-03-02 22:39:12 -0500

    Add files via upload

---
+++ testKLML1D.R
@@ -0,0 +1,30 @@
+# Julio Change Detection
+# testKLML1D
+# 03022021
+# Xiaoyang Chen
+
+
+testKLML1D<-function(parameters){
+
+  inputdata  <- parameters$ML$input 
+  coord  <- parameters$KL$coord 
+  numofpoints  <- length(parameters$KL$coord) 
+  
+   # Add Gaussian "bump" to the data
+  sbump  <- 0.5   # Mid location
+  sigma  <- sqrt(0.001) 
+  maxbump  <- 0.5 
+  bump  <- maxbump*exp( - ((coord-sbump)^2)/(sigma^2)  ) 
+  
+  s1  <- 150/500 
+  s2  <- 350/500 
+  
+  bump[1:round(s1*numofpoints)]  <- 0 
+  bump[round(s2*numofpoints):length(bump)]  <- 0 
+  
+   # Add the bump to the input data
+  parameters$ML$input  <- inputdata + bump 
+
+  
+  return(parameters)
+}
